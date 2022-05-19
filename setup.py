7352e6d1ca44b7b1a18dfed986b85f048e27670a
Shiho Takano <shiho.t0320@gmail.com>
2022-05-19 19:57:42 +0900

    Add: setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='keylogger',
+      version='0.0.1',
+      description='get keyevents',
+      author='astms007',
+      url='https://github.com/artms007/key/',
+      packages=['key'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Framework :: IPython',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Intended Audience :: Education',
+      ],
+)
